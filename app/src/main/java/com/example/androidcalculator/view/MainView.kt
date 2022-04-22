d1f3dd188c0b7fa5d7245dddfb983bfded74a610
コタロー <kk.nnkktr@gmail.com>
2022-04-22 21:59:27 +0900

    選択中の演算子のボタン色が変わるように修正

--- app/src/main/java/com/example/androidcalculator/view/MainView.kt
+++ app/src/main/java/com/example/androidcalculator/view/MainView.kt
@@ -1,24 +1,14 @@
 package com.example.androidcalculator.view
 
-import android.graphics.drawable.Icon
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
-import androidx.compose.material.icons.Icons
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusModifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.androidcalculator.const.Constant
-import com.example.androidcalculator.view.atom.InputButton
-import com.example.androidcalculator.view.organism.ClearButton
-import com.example.androidcalculator.view.organism.NumberButton
-import com.example.androidcalculator.view.organism.OperatorButton
-import com.example.androidcalculator.view.organism.SpecialButton
+import com.example.androidcalculator.view.organism.*
 import com.example.androidcalculator.view.template.Center
 import com.example.androidcalculator.viewmodel.MainViewModel
 
@@ -32,81 +22,102 @@ fun MainView(viewModel: MainViewModel) {
                 }
             }
         ) {
-            Center {
-                Row {
-                    ClearButton(text = "CLEAR")
+            Column {
+                Center(
+                    modifier = Modifier.weight(1F),
+                ) {
+                    NumberDisplay(value = viewModel.displayNumber.value)
                 }
-                Row {
-                    NumberButton(
-                        onClick = {viewModel.onTapNumber("7")},
-                        content = {Text(text = "7")},
-                    )
-                    NumberButton(
-                        onClick = {viewModel.onTapNumber("8")},
-                        content = {Text(text = "8")},
-                    )
-                    NumberButton(
-                        onClick = {viewModel.onTapNumber("9")},
-                        content = {Text(text = "9")},
-                    )
-                    OperatorButton(
-                        onClick = {viewModel.onTapOperator(MainViewModel.CalculateKind.Divide)},
-                        content = {Text(text = "÷")},
-                    )
-                }
-                Row {
-                    NumberButton(
-                        onClick = {viewModel.onTapNumber("4")},
-                        content = {Text(text = "4")},
-                    )
-                    NumberButton(
-                        onClick = {viewModel.onTapNumber("5")},
-                        content = {Text(text = "5")},
-                    )
-                    NumberButton(
-                        onClick = {viewModel.onTapNumber("6")},
-                        content = {Text(text = "6")},
-                    )
-                    OperatorButton(
-                        onClick = {viewModel.onTapOperator(MainViewModel.CalculateKind.Times)},
-                        content = {Text(text = "×")},
-                    )
-                }
-                Row {
-                    NumberButton(
-                        onClick = {viewModel.onTapNumber("1")},
-                        content = {Text(text = "1")},
-                    )
-                    NumberButton(
-                        onClick = {viewModel.onTapNumber("2")},
-                        content = {Text(text = "2")},
-                    )
-                    NumberButton(
-                        onClick = {viewModel.onTapNumber("3")},
-                        content = {Text(text = "3")},
-                    )
-                    OperatorButton(
-                        onClick = {viewModel.onTapOperator(MainViewModel.CalculateKind.Minus)},
-                        content = {Text(text = "-")},
-                    )
-                }
-                Row {
-                    SpecialButton(
-                        onClick = {viewModel.onTapBackspace()},
-                        content = {Text(text = "←")},
-                    )
-                    NumberButton(
-                        onClick = {viewModel.onTapNumber("0")},
-                        content = {Text(text = "0")},
-                    )
-                    OperatorButton(
-                        onClick = {viewModel.onTapEqual()},
-                        content = {Text(text = "=")},
-                    )
-                    OperatorButton(
-                        onClick = {viewModel.onTapOperator(MainViewModel.CalculateKind.Plus)},
-                        content = {Text(text = "+")},
-                    )
+                Center(
+                    modifier = Modifier.weight(2F),
+                ) {
+                    Row {
+                        ClearButton(
+                            onClick = { viewModel.onTapClear() },
+                            text = "CLEAR",
+                        )
+                    }
+                    Row {
+                        NumberButton(
+                            onClick = { viewModel.onTapNumber("7") },
+                            content = { Text(text = "7") },
+                        )
+                        NumberButton(
+                            onClick = { viewModel.onTapNumber("8") },
+                            content = { Text(text = "8") },
+                        )
+                        NumberButton(
+                            onClick = { viewModel.onTapNumber("9") },
+                            content = { Text(text = "9") },
+                        )
+                        OperatorButton(
+                            onClick = { viewModel.onTapOperator(MainViewModel.CalculateKind.Divide) },
+                            operatorKind = MainViewModel.CalculateKind.Divide,
+                            kindState = viewModel.calculateKind.value,
+                            content = { Text(text = "÷") },
+                        )
+                    }
+                    Row {
+                        NumberButton(
+                            onClick = { viewModel.onTapNumber("4") },
+                            content = { Text(text = "4") },
+                        )
+                        NumberButton(
+                            onClick = { viewModel.onTapNumber("5") },
+                            content = { Text(text = "5") },
+                        )
+                        NumberButton(
+                            onClick = { viewModel.onTapNumber("6") },
+                            content = { Text(text = "6") },
+                        )
+                        OperatorButton(
+                            onClick = { viewModel.onTapOperator(MainViewModel.CalculateKind.Times) },
+                            operatorKind = MainViewModel.CalculateKind.Times,
+                            kindState = viewModel.calculateKind.value,
+                            content = { Text(text = "×") },
+                        )
+                    }
+                    Row {
+                        NumberButton(
+                            onClick = { viewModel.onTapNumber("1") },
+                            content = { Text(text = "1") },
+                        )
+                        NumberButton(
+                            onClick = { viewModel.onTapNumber("2") },
+                            content = { Text(text = "2") },
+                        )
+                        NumberButton(
+                            onClick = { viewModel.onTapNumber("3") },
+                            content = { Text(text = "3") },
+                        )
+                        OperatorButton(
+                            onClick = { viewModel.onTapOperator(MainViewModel.CalculateKind.Minus) },
+                            operatorKind = MainViewModel.CalculateKind.Minus,
+                            kindState = viewModel.calculateKind.value,
+                            content = { Text(text = "-") },
+                        )
+                    }
+                    Row {
+                        SpecialButton(
+                            onClick = { viewModel.onTapBackspace() },
+                            content = { Text(text = "←") },
+                        )
+                        NumberButton(
+                            onClick = { viewModel.onTapNumber("0") },
+                            content = { Text(text = "0") },
+                        )
+                        OperatorButton(
+                            onClick = { viewModel.onTapEqual() },
+                            kindState = viewModel.calculateKind.value,
+                            content = { Text(text = "=") },
+                        )
+                        OperatorButton(
+                            onClick = { viewModel.onTapOperator(MainViewModel.CalculateKind.Plus) },
+                            operatorKind = MainViewModel.CalculateKind.Plus,
+                            kindState = viewModel.calculateKind.value,
+                            content = { Text(text = "+") },
+                        )
+                    }
                 }
             }
         }
--- app/src/main/java/com/example/androidcalculator/view/organism/OperatorButton.kt
+++ app/src/main/java/com/example/androidcalculator/view/organism/OperatorButton.kt
@@ -3,14 +3,18 @@ package com.example.androidcalculator.view.organism
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import com.example.androidcalculator.view.atom.InputButton
+import com.example.androidcalculator.viewmodel.MainViewModel
 
 @Composable
 fun OperatorButton(
     onClick: () -> Unit = {},
+    operatorKind: MainViewModel.CalculateKind? = null,
+    kindState: MainViewModel.CalculateKind,
     content: @Composable () -> Unit = {},
 ) {
     InputButton(
-        backgroundColor = Color(0xA2,0xFA,0xDA),
+        backgroundColor = if(operatorKind == kindState) Color(0xEE, 0xFA, 0xA2)
+                            else Color(0xA2,0xFA,0xDA),
         onClick = onClick,
         content = content,
     )
